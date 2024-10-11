3a38dbce883f6fbe688acd59842d49b4eef0ca70
vanstrouble <vanstrouble@proton.me>
2024-10-11 16:28:42 -0600

    Create data folder with categories.js

---
+++ snack-attack-react/src/data/categories.js
@@ -0,0 +1,36 @@
+const categories = [
+    {
+        icon: "coffee",
+        name: "Coffee",
+        id: 1
+      },
+      {
+        icon: "hamburger",
+        name: "Hamburgers",
+        id: 2
+      },
+      {
+        icon: "pizza",
+        name: "Pizzas",
+        id: 3
+      },
+      {
+        icon: "donut",
+        name: "Donuts",
+        id: 4
+      },
+      {
+        icon: "cake",
+        name: "Cakes",
+        id: 5
+      },
+      {
+        icon: "cookies",
+        name: "Cookies",
+        id: 6
+      }
+]
+
+export {
+    categories
+}
