8494b9145f82873f4616a55d4b6812dd159cf623
vanstrouble <vanstrouble@proton.me>
2024-10-10 19:22:54 -0600

    Fix width main

--- snack-attack-react/src/layouts/AuthLayout.jsx
+++ snack-attack-react/src/layouts/AuthLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 
 export default function AuthLayout() {
     return (
-        <main className="max-w-xl m-auto mt-10 md:mt-28 flex flex-col md:flex-row items-center">
+        <main className="max-w-4xl m-auto mt-10 md:mt-28 flex flex-col md:flex-row items-center">
             <img
                 src="../img/logo.svg"
                 alt="Logo image"
