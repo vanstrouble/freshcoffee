6cbf2571d656f7919142a66334bbb7d430fcc84f
vanstrouble <vanstrouble@proton.me>
2024-10-10 18:47:04 -0600

    Add logo img to login page

--- snack-attack-react/src/layouts/AuthLayout.jsx
+++ snack-attack-react/src/layouts/AuthLayout.jsx
@@ -2,9 +2,16 @@ import { Outlet } from "react-router-dom"
 
 export default function AuthLayout() {
     return (
-        <div>
-            AuthLayout
-            <Outlet />
-        </div>
+        <main className="max-w-xl m-auto mt-10 md:mt-28 flex flex-col md:flex-row items-center">
+            <img
+                src="../img/logo.svg"
+                alt="Logo image"
+                className="max-w-xs"
+            />
+
+            <div className="p-10 w-full">
+                <Outlet />
+            </div>
+        </main>
     )
 }
