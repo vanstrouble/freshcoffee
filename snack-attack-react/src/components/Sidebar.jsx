e55a63e02cdc62e3addf22e6098c1ba8d41e7c6a
vanstrouble <vanstrouble@proton.me>
2024-10-11 16:29:32 -0600

    Get and show categories from categories.js in Sidebar

--- snack-attack-react/src/components/Sidebar.jsx
+++ snack-attack-react/src/components/Sidebar.jsx
@@ -1,7 +1,21 @@
+import { categories } from "../data/categories"
+
 export default function Sidebar() {
     return (
-        <div className="md:w-72">
-            Sidebar
-        </div>
+        <aside className="md:w-72">
+            <div className="p-4">
+                <img
+                    className="w-40"
+                    src="img/logo.svg"
+                    alt="Logo image"
+                />
+            </div>
+
+            <div className="mt-10">
+                {categories.map(category => (
+                    <p key={category.id}>{category.name}</p>
+                ))}
+            </div>
+        </aside>
     )
 }
