27922815eb782e0454c69fe7121470797fbdd8cd
vanstrouble <vanstrouble@proton.me>
2024-10-10 18:09:51 -0600

    Create Auth Layout

--- snack-attack-react/src/router.jsx
+++ snack-attack-react/src/router.jsx
@@ -1,10 +1,33 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Layout from './layouts/Layout'
+import AuthLayout from './layouts/AuthLayout'
+import Login from './views/Login'
+import Register from './views/Register'
 
 const Router = createBrowserRouter([
     {
         path: '/',
-        element: <Layout />
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <div>Home</div>
+            }
+        ]
+    },
+    {
+        path: '/auth',
+        element: <AuthLayout />,
+        children: [
+            {
+                path: '/auth/login',
+                element: <Login />
+            },
+            {
+                path: '/auth/register',
+                element: <Register />
+            }
+        ]
     }
 ])
 
