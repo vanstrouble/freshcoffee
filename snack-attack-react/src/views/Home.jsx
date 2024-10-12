4bff5d4d942294aa4f1e3550c456a2e7c9611fa6
vanstrouble <vanstrouble@proton.me>
2024-10-12 16:35:30 -0600

    Get all products from products.js

--- snack-attack-react/src/views/Home.jsx
+++ snack-attack-react/src/views/Home.jsx
@@ -1,5 +1,23 @@
+import Product from '../components/Product'
+import { products } from '../data/products'
+
 export default function Home() {
+    console.log(products)
     return (
-        <div>Home</div>
+        <>
+            <h1 className='text-4xl font-black'>Home</h1>
+            <p className='text-2xl my-10'>
+                Choose and customize your order below
+            </p>
+
+            <div className='grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
+                {products.map((product) => (
+                    <Product
+                    key={product.image}
+                    product={product}
+                    />
+                ))}
+            </div>
+        </>
     )
 }
