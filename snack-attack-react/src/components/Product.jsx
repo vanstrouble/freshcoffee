50555b0b66333bd1a054241fb66ca773241ceccb
vanstrouble <vanstrouble@proton.me>
2024-10-13 15:44:16 -0600

    Show all products with their details

--- snack-attack-react/src/components/Product.jsx
+++ snack-attack-react/src/components/Product.jsx
@@ -1,5 +1,40 @@
+import { formatPrice } from "../helpers"
+import PropTypes from 'prop-types';
 export default function Product({ product }) {
+
+    const { image, name, price } = product
+
     return (
-        <div>Product</div>
+        <div className="border p-3 shadow bg-white rounded-sm flex flex-col justify-between h-full">
+            <img
+                src={`/img/${image}.jpg`}
+                alt={`${name} image`}
+                className="w-full"
+            />
+
+            <div className="p-5 flex flex-col flex-grow justify-between">
+                <h3 className="text-3xl font-bold">{name}</h3>
+
+                <div className="mt-5">
+                    <p className="font-black text-4xl text-amber-500">
+                        { formatPrice(price) }
+                    </p>
+
+                    <button
+                        className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold rounded-md"
+                    >
+                        Add to cart
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
+
+Product.propTypes = {
+    product: PropTypes.shape({
+        image: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+    }).isRequired,
+};
--- snack-attack-react/src/views/Home.jsx
+++ snack-attack-react/src/views/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
         <>
             <h1 className='text-4xl font-black'>Home</h1>
             <p className='text-2xl my-10'>
-                Choose and customize your order below
+                Choose and customize your order below:
             </p>
 
             <div className='grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
