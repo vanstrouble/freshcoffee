7e26a5e67af8965dfd4f576a441b5e6157daa291
vanstrouble <vanstrouble@proton.me>
2024-10-18 16:12:01 -0600

    Refactor CategoryController to add index method

--- snack-attack-laravel/routes/web.php
+++ snack-attack-laravel/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/categories', [CategoryController::class, 'index']);
