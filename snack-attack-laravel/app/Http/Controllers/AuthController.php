caaa2b10af7f551010918cc47ef53e5e31e139ac
vanstrouble <vanstrouble@proton.me>
2024-10-22 20:28:49 -0600

    Refactor authentication configuration to enable support for credentials in API requests

--- snack-attack-laravel/app/Http/Controllers/AuthController.php
+++ snack-attack-laravel/app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -23,5 +24,16 @@ class AuthController extends Controller
 
     public function register(RegisterRequest $request) {
         $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        return [
+            'token' => $user->createToken('auth_token')->plainTextToken,
+            'user' => $user
+        ];
     }
 }
--- snack-attack-react/src/config/axios.js
+++ snack-attack-react/src/config/axios.js
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        "Accept": "application/json",
+        "X-Requested-With": "XMLHttpRequest"
+    },
+    withCredentials: true
 });
 
 export default axiosInstance;
