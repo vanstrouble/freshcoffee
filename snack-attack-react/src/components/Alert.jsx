0fa3cf5d37ead9bce2e2c48e9cae2f28962719b2
vanstrouble <vanstrouble@proton.me>
2024-10-22 19:09:32 -0600

    Refactor Alert component to include PropTypes validation

--- snack-attack-react/src/components/Alert.jsx
+++ snack-attack-react/src/components/Alert.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export default function Alert({ children }) {
     return (
         <div className="bg-red-100 border border-red-400 text-red-700 my-2 px-4 py-3 rounded relative" role="alert">
@@ -5,3 +7,7 @@ export default function Alert({ children }) {
         </div>
     );
 }
+
+Alert.propTypes = {
+    children: PropTypes.node.isRequired,
+};
