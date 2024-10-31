0f2688c76abee0357f471a942af28832bdb34ea8
vanstrouble <vanstrouble@proton.me>
2024-10-30 19:40:40 -0600

    Add logout button to AdminSidebar and enhance navigation links

--- snack-attack-react/src/components/AdminSidebar.jsx
+++ snack-attack-react/src/components/AdminSidebar.jsx
@@ -1,7 +1,33 @@
-export default function AdminSidebar() {
-    return (
-        <aside className="md:w-72 h-screen">
+import { Link } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
-        </aside>
-    )
+export default function AdminSidebar() {
+    const { logout } = useAuth({middleware: 'auth'});
+
+	return (
+		<aside className="md:w-72 h-screen">
+			<div className="p-4">
+				<img src="/img/logo.svg" alt="Logo image" className="w-40" />
+			</div>
+
+			<nav className="flex flex-col p-4">
+				<Link to="/admin" className="font-bold text-lg">
+					Orders
+				</Link>
+				<Link to="/admin/products" className="font-bold text-lg">
+					Products
+				</Link>
+			</nav>
+
+			<div className="my-5 px-5">
+				<button
+					type="button"
+					className="text-center bg-red-500 w-full p-3 font-bold text-white truncate rounded-md"
+					onClick={logout}
+				>
+					Logout
+				</button>
+			</div>
+		</aside>
+	);
 }
