319c44af94edc72ab1f3b4c21cd11f18a030f863
vanstrouble <vanstrouble@proton.me>
2024-11-05 14:55:41 -0600

    Implement order fetching in Orders component and update OrderController index method

--- snack-attack-react/src/views/Orders.jsx
+++ snack-attack-react/src/views/Orders.jsx
@@ -1,10 +1,23 @@
+import useSWR from "swr";
+import axiosInstance from "../config/axios";
+
 export default function Orders() {
+	const token = localStorage.getItem("AUTH_TOKEN");
+	const fetcher = () =>
+		axiosInstance("/api/orders", {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+	const { data, error, isLoading } = useSWR("/api/orders", fetcher);
+
+	console.log(data, error, isLoading);
+
 	return (
 		<div>
 			<h1 className="text-4xl font-black">Ordens</h1>
-			<p className="text-2xl my-10">
-                Manage the orders from here
-			</p>
+			<p className="text-2xl my-10">Manage the orders from here</p>
 		</div>
 	);
 }
